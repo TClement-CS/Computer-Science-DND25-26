9701e6907e9721726a72653cd749d1008f1c96c6
TClement-CS <tclement1@hwemail.com>
2025-10-20 11:15:58 -0700

    class work
    recursion

---
+++ Unit-3/RecursiveListTester.java
@@ -0,0 +1,19 @@
+public class RecursiveListTester {
+    public static void main(String[] args) {
+        ListNode a = new ListNode("A");
+        ListNode b = new ListNode("B");
+        ListNode c = new ListNode("C");
+        ListNode d = new ListNode("D");
+        ListNode e = new ListNode("E");
+        ListNode f = new ListNode("F");
+        ListNode g = new ListNode("G");
+        a.setNext(b);
+        b.setNext(c);
+        c.setNext(d);
+        d.setNext(e);
+        e.setNext(f);
+        f.setNext(g);
+        g.setNext(null);
+        Recursion.printListInReverse(a);
+    }
+}
