b1840d4b4fc198a37e3ea7e378d6748bfc01a12f
TClement-CS <tclement1@hwemail.com>
2025-11-18 08:38:56 -0800

    nothing

--- Unit-0/PugRescue 2/PugSaverTester.java
+++ Unit-0/PugRescue 2/PugSaverTester.java
@@ -1,12 +1,18 @@
-// import java.util.ArrayList;
+import java.util.ArrayList;
 
-// public class PugSaverTester {
-//     public static void main(String[] args) {
-//         Dog A = new Dog("German Shepard");
-//         Dog B = new Dog("Golden Retriever");
-//         Dog C = new Dog("Corgi");
-//         Dog D = new Dog("Border Collie");
-//         Dog E = new Dog("Golden Doodle");
-//         ArrayList<Dog> list1 = new ArrayList<>();
-//     }
-// }
+public class PugSaverTester {
+    public static void main(String[] args) {
+        Dog A = new Dog("German Shepard");
+        Dog B = new Dog("Golden Retriever");
+        Dog C = new Dog("Corgi");
+        Dog D = new Dog("Border Collie");
+        Dog E = new Dog("Golden Doodle");
+        MyArrayList<Dog> list1 = new MyArrayList<>();
+        list1.add(A);
+        list1.add(B);
+        list1.add(C);
+        list1.add(D);
+        list1.add(E);
+        list1.remove(B);
+    }
+}
