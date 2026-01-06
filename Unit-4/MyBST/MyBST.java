248ac0ecf15699a417b24a99e054fcf8107848ee
TClement-CS <tclement1@hwemail.com>
2026-01-06 09:12:42 -0800

    In class work 1-6

---
+++ Unit-4/MyBST/MyBST.java
@@ -0,0 +1,101 @@
+// Implements a BST with BinaryNode nodes
+
+public class MyBST<E extends Comparable<E>> {
+
+	private BinaryNode<E> root; // holds the root of this BST
+
+	// Constructor: creates an empty BST.
+	public MyBST() {
+		root = null;
+	}
+
+	public BinaryNode<E> getRoot() {
+		return root;
+	}
+
+	public int getHeight() {
+		return root.getHeight();
+	}
+
+	// Returns true if this BST contains value; otherwise returns false.
+	public boolean contains(E value) {
+		BinaryNode<E> val = new BinaryNode<E>(value);
+		BinaryNode<E> temp = root;
+		if (value.compareTo(root.getValue()) > 0) {
+			if (!temp.hasRight()) {
+				root.setRight(val);
+			}
+		}
+		if (value.compareTo(root.getValue()) < 0) {
+			if (!temp.hasLeft()) {
+				root.setLeft(val);
+			}
+		}
+		
+		return false;
+	}
+
+	// Adds value to this BST, unless this tree already holds value.
+	// Returns true if value has been added; otherwise returns false.
+	public boolean add(E value) {
+		return false;
+	}
+
+	// Removes value from this BST. Returns true if value has been
+	// found and removed; otherwise returns false.
+	// If removing a node with two children: replace it with the
+	// largest node in the right subtree
+	public boolean remove(E value) {
+		return false;
+	}
+
+	// Returns the minimum in the tree
+	public E min() {
+		return minHelp(root);
+	}
+
+	public E minHelp(BinaryNode<E> node) {
+		if (node.hasLeft()) {
+			minHelp(node.getLeft());
+		}
+		return node.getValue();
+	}
+
+	// Returns the maximum in the tree.
+	public E max() {
+		return maxHelp(root);
+	}
+
+	public E maxHelp(BinaryNode<E> node) {
+		if (node.hasRight()) {
+			minHelp(node.getRight());
+		}
+		return node.getValue();
+	}
+
+	// Returns a bracket-surrounded, comma separated list of the contents of the
+	// nodes, in order
+	// e.g. [Apple, Cranberry, Durian, Mango]
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		toStringHelper(root, sb);
+		sb.append("]");
+		return sb.toString();
+
+	}
+
+	public StringBuilder toStringHelper(BinaryNode<E> node, StringBuilder sb) {
+		if (node.hasLeft()) {
+			toStringHelper(node.getLeft(), sb);
+		}
+
+		sb.append(node.getValue() + ", ");
+
+		if (node.hasRight()) {
+			toStringHelper(node.getRight(), sb);
+		}
+		return sb;
+	}
+
+}
