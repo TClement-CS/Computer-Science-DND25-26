853d106d43ca9a9c711d872702b83d03b4e1f103
TClement-CS <tclement1@hwemail.com>
2026-02-10 09:30:56 -0800

    February 10, 2026

---
+++ Unit-6/HeapPQ/HeapPQ.java
@@ -0,0 +1,85 @@
+
+public class HeapPQ<E extends Comparable<E>> implements MyPriorityQueue<E> {
+
+	private E[] heap;
+	private int objectCount;
+
+	public HeapPQ() {
+		this.heap = (E[]) new Comparable[3];
+		this.objectCount = 0;
+	}
+
+	// Returns the number of elements in the priority queue
+	public int size() {
+		return objectCount;
+	}
+
+	// DO NOT CHANGE MY JANKY TOSTRING!!!!!
+	public String toString() {
+		StringBuffer stringbuf = new StringBuffer("[");
+		for (int i = 0; i < objectCount; i++) {
+			stringbuf.append(heap[i]);
+			if (i < objectCount - 1)
+				stringbuf.append(", ");
+		}
+		stringbuf.append("]\nor alternatively,\n");
+
+		for (int rowLength = 1, j = 0; j < objectCount; rowLength *= 2) {
+			for (int i = 0; i < rowLength && j < objectCount; i++, j++) {
+				stringbuf.append(heap[j] + " ");
+			}
+			stringbuf.append("\n");
+			if (j < objectCount) {
+				for (int i = 0; i < Math.min(objectCount - j, rowLength * 2); i++) {
+					if (i % 2 == 0)
+						stringbuf.append("/");
+					else
+						stringbuf.append("\\ ");
+				}
+				stringbuf.append("\n");
+			}
+		}
+		return stringbuf.toString();
+	}
+
+	// Doubles the size of the heap array
+	private void increaseCapacity() {
+		E[] newHeap = (E[]) new Comparable[objectCount * 2];
+		for (int i = 0; i < objectCount; i++) {
+			newHeap[i] = heap[i];
+		}
+
+	}
+
+	// Returns the index of the "parent" of index i
+	private int parent(int i) {
+		return (i + 1) / 2 - 1;
+	}
+
+	// Returns the index of the *smaller child* of index i
+	private int smallerChild(int i) {
+		return (i + 1) * 2 - 1;
+	}
+
+	// Swaps the contents of indices i and j
+	private void swap(int i, int j) {
+		if (i > objectCount || i < 0 || j > objectCount || j < 0) {
+			throw new IndexOutOfBoundsException();
+		}
+		E temp = heap[i];
+		heap[i] = heap[j];
+		heap[j] = temp;
+	}
+
+	// Bubbles the element at index i upwards until the heap properties hold again.
+	private void bubbleUp(int i) {
+
+	}
+
+	// Bubbles the element at index i downwards until the heap properties hold
+	// again.
+	private void bubbleDown(int i) {
+
+	}
+
+}
