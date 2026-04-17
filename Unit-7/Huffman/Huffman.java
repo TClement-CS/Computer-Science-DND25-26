ef4b2d8ab39772d3fde16a8871362b6c289da2bd
TClement-CS <tclement1@hwemail.com>
2026-04-17 11:15:33 -0700

    huffman step 1

---
+++ Unit-7/Huffman/Huffman.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class Huffman {
+
+    public HashMap<Character, Integer> frequencyFinder(String strFile) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(strFile));
+        HashMap<Character, Integer> frequencyTable = new HashMap<>();
+        while (br.ready()) {
+            char c = (char) br.read();
+            if (frequencyTable.containsKey(c)) {
+                int frequency = frequencyTable.get(c);
+                frequency++;
+                frequencyTable.put(c, frequency);
+            } else {
+                frequencyTable.put(c, 1);
+            }
+        }
+        return frequencyTable;
+
+    }
+}
