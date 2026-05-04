7714d38a50573ef0c8c99af0536836e5c0958090
TClement-CS <tclement1@hwemail.com>
2026-05-04 10:38:16 -0700

    Create HuffmanEncoder.java

---
+++ Unit-7/Huffman/HuffmanEncoder.java
@@ -0,0 +1,15 @@
+public class HuffmanEncoder {
+
+    public HuffmanEncoder(String codeFile) {
+
+    }
+
+    public String encodeChar(char input) {
+
+    }
+
+    public void encodeFileToHuffmanCodes(String fileToCompress, String encodedFile) {
+        
+    }
+}
+\\
